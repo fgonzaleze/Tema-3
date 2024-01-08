1d76cef2875a48caef0b594f4db3824732b4d6a7
jgonzalez <jgonzaleze@iesnervion.es>
2024-01-08 14:37:39 +0100

    Ejercicio 1 completo

---
+++ EjerciciosParte1/ejercicio3.py
@@ -0,0 +1,10 @@
+# Número oculto
+# Se debe generar un número al azar entre cero y cien, que deben intentar adivinar otros diez hilos. 
+# El número a adivinar se almacenará en una variable de clase. 
+# Dentro del método run() se debe implementar la siguiente lógica:
+# El hilo genera un número aleatorio.
+# Se comprueba si el número generado coincide con el que hay que acertar:
+# En caso afirmativo, se termina la ejecución del hilo.
+# En caso contrario, hay que comprobar si otro hilo ya ha acertado. Para ello habrá que crear otra variable de clase que lo controle:
+# Si otro hilo ya ha acertado, entonces se termina la ejecución del hilo.
+# Si todavía nadie ha acertado, se sigue con la búsqueda del número a acertar.
