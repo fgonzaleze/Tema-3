fc79aa9eda6a492cd007c6c5244d98f89977369d
jgonzalez <jgonzaleze@iesnervion.es>
2024-01-22 14:17:16 +0100

    Ejemplo sobre los semáforos

---
+++ ejemploSemaforo/ejemploSemaforo.py
@@ -0,0 +1,20 @@
+
+
+import random
+from threading import Semaphore, Thread
+import time
+
+
+class Cajero(Thread):
+    semaforo = Semaphore(4)
+
+    def __init__(self,nombre):
+        Thread.__init__(self, name = nombre)
+    
+    def run(self):
+        print("El hilo", self.name, "está comprando")
+        Cajero.semaforo.acquire()
+        print("El hilo", self.name, "está en caja")
+        time.sleep(random.randint(1,10))
+        print("El hilo", self.name, "ya ha pagado")
+        Cajero.semaforo.release()
---
+++ ejemploSemaforo/main.py
@@ -0,0 +1,14 @@
+from ejemploSemaforo.ejemploSemaforo import Cajero
+
+
+if __name__ == "__main__":
+    lista = []
+    for i in range(20):
+        hilo = Cajero(str(i))
+        hilo.start()
+        lista.append(hilo)
+    
+    for h in lista:
+        h.join()
+    
+    print("Fin del main")
