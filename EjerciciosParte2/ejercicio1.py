af1b22e83c5e36f67797e37060e1eaa74c3582a4
jgonzalez <jgonzaleze@iesnervion.es>
2024-01-23 11:11:28 +0100

    Ejercicio 1 no me sale

--- EjerciciosParte2/ejercicio1.py
+++ EjerciciosParte2/ejercicio1.py
@@ -1,3 +1,29 @@
 # Vuelve a realizar este ejercicio de la PARTE 1 pero utilizando Lock() como método de sincronización para asegurarte de que una vez que se averigüe el número, 
 # el resto de hilos no generan de nuevo el número.
 
+from threading import Lock, Thread
+import random
+
+class NumOculto(Thread):
+    numAdivinar = random.randint(0,100)
+    numAcertado = False
+    lock = Lock()
+
+    def __init__(self, nombre):
+        Thread.__init__(self, name = nombre)
+    
+    def run(self):
+        
+        while True:
+            numAleatorio = random.randint(0,100)
+            NumOculto.lock.acquire()
+            if numAleatorio == NumOculto.numAdivinar:
+                
+                print("He acertado, soy el hilo", self.name, "el resultado; numAleatorio:", numAleatorio, "numAdivinar", NumOculto.numAdivinar)
+                NumOculto.numAcertado = True
+                break
+            
+            elif NumOculto.numAcertado == True:
+                print("Ya se ha acertado el numero")
+            NumOculto.lock.release()
+            
---
+++ EjerciciosParte2/main1.py
@@ -0,0 +1,6 @@
+from ejercicio1 import NumOculto
+
+if __name__ == "__main__":
+     for i in range(0,10):
+         hilo = NumOculto(f"hilo{i}")
+         hilo.start()
